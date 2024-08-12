a90c26a08249dda039db94e34398647977d5eb7c
haena7908 <yhn7908@gmail.com>
2024-08-12 13:40:03 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,12 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4AHe9j6lhrE3AD21d2WuS44ctIKPp0os",
+  authDomain: "messenger-retry.firebaseapp.com",
+  databaseURL:
+    "https://messenger-retry-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "messenger-retry",
+  storageBucket: "messenger-retry.appspot.com",
+  messagingSenderId: "332064995660",
+  appId: "1:332064995660:web:7aa2d29e7b773b39f677e9",
+  measurementId: "",
+};
